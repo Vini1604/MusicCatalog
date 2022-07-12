fe0430aeb28e9cd5823dc086df81a17e085c6284
Vini1604 <vinicius.oliveira1998@hotmail.com>
2022-07-12 12:29:45 -0300

    Saved controller

--- MusicCatalogAPI/Controllers/MusicController.cs
+++ MusicCatalogAPI/Controllers/MusicController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MusicCatalogAPI.Models.Dtos;
+using MusicCatalogAPI.Repository.IRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +14,37 @@ namespace MusicCatalogAPI.Controllers
     [ApiController]
     public class MusicController : ControllerBase
     {
+        private IMusicRepository _musicRepository;
+        private readonly IMapper _mapper;
+        public MusicController(IMusicRepository musicRepository, IMapper mapper)
+        {
+            _musicRepository = musicRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetMusics()
+        {
+            var musics = _musicRepository.GetMusics();
+            var musicsDto = new List<MusicDto>();
+
+            foreach (var music in musics)
+            {
+                musicsDto.Add(_mapper.Map<MusicDto>(music));
+            }
+            return Ok(musicsDto);
+        }
+
+        [HttpGet("{musicId:int}")]
+        public IActionResult GetMusic(int musicId)
+        {
+            var music = _musicRepository.GetMusic(musicId);
+            if (music == null)
+            {
+                return NotFound();
+            }
+            var musicDto = _mapper.Map<MusicDto>(music);
+            return Ok(musicDto);
+        }
     }
 }
